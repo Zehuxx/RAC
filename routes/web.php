1aebac940f011758d1134773e90d67aff53aa68d
Guillermo Cruz <cruzguillermo754@gmail.com>
2019-09-01 10:00:55 -0600

    routes

--- routes/web.php
+++ routes/web.php
@@ -16,20 +16,26 @@ Route::get('/', function () {
 })->name('home');
 
 //RUTAS ADMINS
-Route::view('/admin', 'admin/home')->name('admin home');
-Route::view('/admin/empleados', 'admin/employes')->name('admin employes');
-Route::view('/admin/empleados/add', 'admin/add_employe')->name('admin employes add');
+Route::group(['prefix' => 'admin'], function () {
+    Route::view('/', 'admin/home')->name('admin home');
+    Route::view('empleados', 'admin/employes')->name('admin employes');
+    Route::view('empleados/add', 'admin/add_employe')->name('admin employes add');
 
-Route::view('/admin/modelos', 'admin/models')->name('admin models');
-Route::view('/admin/modelos/add', 'admin/add_model')->name('admin models add');
+    Route::view('modelos', 'admin/models')->name('admin models');
+    Route::view('modelos/add', 'admin/add_model')->name('admin models add');
 
-Route::view('/admin/tipos', 'admin/types')->name('admin types');
-Route::view('/admin/tipos/add', 'admin/add_type')->name('admin types add');
+    Route::view('tipos', 'admin/types')->name('admin types');
+Route::view('tipos/add', 'admin/add_type')->name('admin types add');
+});
+
 //RUTAS USERS
-Route::view('/user', 'user/home')->name('user home');
-Route::view('/detalles', 'user/details')->name('user detalles');
-Route::view('/orden', 'user/new_order')->name('user orden');
-Route::view('/carro', 'user/new_car')->name('user carro');
-Route::view('/ordenes', 'user/order_view');
-Route::view('/clientes', 'user/client_view');
-Route::view('/cliente', 'user/new_client');
+Route::group(['prefix' => 'user'], function () {
+
+    Route::view('user', 'user/home')->name('user home');
+    Route::view('detalles', 'user/details')->name('user detalles');
+    Route::view('orden', 'user/new_order')->name('user orden');
+    Route::view('carro', 'user/new_car')->name('user carro');
+    Route::view('ordenes', 'user/order_view');
+    Route::view('clientes', 'user/client_view');
+    Route::view('cliente', 'user/new_client');
+});

31fcad96786a2e79bfcb95d1fc85820d9b66648a
Guillermo Cruz <cruzguillermo754@gmail.com>
2019-09-09 21:56:41 -0600

    fields

--- resources/views/admin/add_employe.blade.php
+++ resources/views/admin/add_employe.blade.php
@@ -22,31 +22,51 @@
                 <div class="form-group row">
                     <label class="col-md-3 col-form-label" for="nombre">Nombre</label>
                     <div class="col-md-9">
-                    <input class="form-control" id="nombre" type="text" name="nombre" placeholder="{{$holders['name']}}" required>
-                        <span class="help-block">Su nombre</span>
+                    <input class="form-control" id="nombre" type="text" name="nombre" value="{{ old('nombre') }}" placeholder="{{$holders['name']}}" required>
+                    @if($errors->has('nombre'))
+                    <div class="alert alert-danger">
+                        <span>*{{ $errors->first('nombre') }}</span>
+                    </div>
+                    @endif    
+                    <span class="help-block">Su nombre</span>
                     </div>
                 </div>
 
                 <div class="form-group row">
                     <label class="col-md-3 col-form-label" for="apellido">Apellido</label>
                     <div class="col-md-9">
-                    <input class="form-control" id="apellido" type="text" name="apellido" placeholder="{{$holders['last']}}" required>
-                        <span class="help-block">Porfavor introduzca su apellido</span>
+                    <input class="form-control" id="apellido" type="text" name="apellido" value="{{ old('apellido') }}" placeholder="{{$holders['last']}}" required>
+                    @if($errors->has('apellido'))
+                    <div class="alert alert-danger">
+                        <span>*{{ $errors->first('apellido') }}</span>
+                    </div>
+                    @endif    
+                    <span class="help-block">Porfavor introduzca su apellido</span>
                     </div>
                 </div>
 
                 <div class="form-group row">
                     <label class="col-md-3 col-form-label" for="identidad">Identidad</label>
                     <div class="col-md-9">
-                    <input class="form-control" id="identidad" type="text" name="identidad" placeholder="{{$holders['id']}}" required>
-                        <span class="help-block">Identidad</span>
+                    <input class="form-control" id="identidad" type="text" name="identidad" value="{{ old('identidad') }}" placeholder="{{$holders['id']}}" required>
+                    @if($errors->has('identidad'))
+                    <div class="alert alert-danger">
+                        <span>*{{ $errors->first('identidad') }}</span>
+                    </div>
+                    @endif    
+                    <span class="help-block">Identidad</span>
                     </div>
                 </div>
 
                 <div class="form-group row">
                     <label class="col-md-3 col-form-label" for="telefono">Teléfono</label>
                     <div class="col-md-9">
-                        <input class="form-control" id="telefono" type="text" name="telefono" placeholder="{{$holders['tel']}}" required>
+                        <input class="form-control" id="telefono" type="text" name="telefono" value="{{ old('telefono') }}" placeholder="{{$holders['tel']}}" required>
+                        @if($errors->has('telefono'))
+                        <div class="alert alert-danger">
+                            <span>*{{ $errors->first('telefono') }}</span>
+                        </div>
+                        @endif
                         <span class="help-block">Teléfono</span>
                     </div>
                 </div>
@@ -54,7 +74,7 @@
                 <div class="form-group row">
                     <label class="col-md-3 col-form-label" for="direccion">Dirección</label>
                     <div class="col-md-9">
-                        <input class="form-control" id="direccion" type="text" name="direccion" placeholder="{{$holders['addr']}}" required>
+                        <input class="form-control" id="direccion" type="text" name="direccion" value="{{ old('direccion') }}" placeholder="{{$holders['addr']}}" required>
                         <span class="help-block">Dirección</span>
                     </div>
                 </div>
@@ -62,7 +82,7 @@
                 <div class="form-group row">
                     <label class="col-md-3 col-form-label" for="sexo">Sexo</label>
                     <div class="col-md-9">
-                        <select class="form-control" id="sexo" name="sexo">
+                        <select class="form-control" id="sexo" name="sexo" value="{{ old('sexo') }}">
                             <option value="0">Seleccionar...</option>
                             <option value="1">M</option>
                             <option value="2">F</option>
@@ -73,7 +93,7 @@
                 <div class="form-group row">
                     <label class="col-md-3 col-form-label" for="fecha-nacimiento">Fecha de nacimiento</label>
                     <div class="col-md-9">
-                        <input class="form-control" id="fecha-nacimiento" type="date" name="fecha-nacimiento" placeholder="{{$holders['birthdate']}}" required>
+                        <input class="form-control" id="fecha-nacimiento" type="date" name="fecha-nacimiento" value="{{ old('fecha-nacimiento') }}" placeholder="{{$holders['birthdate']}}" required>
                         <span class="help-block">Please enter a valid date</span>
                     </div>
                 </div>
@@ -81,7 +101,7 @@
                 <div class="form-group row">
                     <label class="col-md-3 col-form-label" for="rol">Rol</label>
                     <div class="col-md-9">
-                        <select class="form-control" id="rol" name="rol">
+                        <select class="form-control" id="rol" name="rol" value="{{ old('rol') }}">
                             <option value="0">Seleccionar...</option>
                             <option value="1">Asistente</option>
                             <option value="2">User</option>
@@ -93,7 +113,7 @@
                 <div class="form-group row">
                     <label class="col-md-3 col-form-label" for="salario">Salario</label>
                     <div class="col-md-9">
-                        <input class="form-control" id="salario" type="text" name="salario" placeholder="{{$holders['salary']}}" required>
+                        <input class="form-control" id="salario" type="text" name="salario" value="{{ old('salario') }}" placeholder="{{$holders['salary']}}" required>
                         <span class="help-block">Salario</span>
                     </div>
                 </div>
@@ -108,14 +128,14 @@
                             <div class="form-group row">
                                 <label class="col-md-3 col-form-label" for="comision">Comision</label>
                                 <div class="col-md-9">
-                                    <input class="form-control" id="comision" type="text" name="comision" placeholder="{{$holders['commission']}}">
+                                    <input class="form-control" id="comision" type="text" name="comision" value="{{ old('comision') }}" placeholder="{{$holders['commission']}}">
                                     <span class="help-block">Comision</span>
                                 </div>
                             </div>
                             <div class="form-group row">
                                 <label class="col-md-3 col-form-label" for="meta">Meta</label>
                                 <div class="col-md-9">
-                                    <input class="form-control" id="meta" type="text" name="meta" placeholder="{{$holders['goal']}}">
+                                    <input class="form-control" id="meta" type="text" name="meta" value="{{ old('meta') }}" placeholder="{{$holders['goal']}}">
                                     <span class="help-block">Meta</span>
                                 </div>
                             </div>
@@ -126,7 +146,7 @@
                 <div class="form-group row">
                     <label class="col-md-3 col-form-label" for="Email">Email</label>
                     <div class="col-md-9">
-                        <input class="form-control" id="Email" type="email" name="Email" placeholder="{{$holders['email']}}" required>
+                        <input class="form-control" id="Email" type="email" name="Email" value="{{ old('Email') }}" placeholder="{{$holders['email']}}" required>
                         <span class="help-block">Correo electronico</span>
                     </div>
                 </div>

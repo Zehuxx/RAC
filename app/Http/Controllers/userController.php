433cba8b0cce25522036b980be3424a3a5499550
Guillermo Cruz <cruzguillermo754@gmail.com>
2019-09-27 09:29:45 -0600

    swagger

--- app/Http/Controllers/userController.php
+++ app/Http/Controllers/userController.php
@@ -8,8 +8,83 @@ use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
+/**
+* @OA\Info(title="Api RAC", version="1.0")
+*
+* @OA\Server(url="http://127.0.0.1:8000")
+*/
+
 class userController extends Controller
 {
+/**
+    * @OA\Post(
+    *     path="api/Signup",
+    *     summary="SignUp usuarios",
+    *     @OA\Parameter(
+    *         name="email",
+    *         in="query",
+    *         description="Correo electronico",
+    *         required=true,        
+    *     ),
+    *     @OA\Parameter(
+    *         name="password",
+    *         in="query",
+    *         description="Contraseña",
+    *         required=true,        
+    *     ),
+    *     @OA\Parameter(
+    *         name="name",
+    *         in="query",
+    *         description="Nombre de pila",
+    *         required=true,        
+    *     ),
+    *     @OA\Parameter(
+    *         name="last_name",
+    *         in="query",
+    *         description="Apellido",
+    *         required=true,        
+    *     ),
+        *     @OA\Parameter(
+    *         name="birth_date",
+    *         in="query",
+    *         description="Fecha de nacimiento formato: YYYY-MM-DD",
+    *         required=true,        
+    *     ),
+        *     @OA\Parameter(
+    *         name="phone",
+    *         in="query",
+    *         description="Numero de telefono formato: NNNNN-NNNNN",
+    *         required=true,        
+    *     ),
+        *     @OA\Parameter(
+    *         name="identification_card",
+    *         in="query",
+    *         description="Tarjeta de identidad. Formato: NNNN-NNNN-NNNNN",
+    *         required=true,        
+    *     ),
+        *     @OA\Parameter(
+    *         name="gender",
+    *         in="query",
+    *         description="Genero",
+    *         required=true,        
+    *     ),
+        *     @OA\Parameter(
+    *         name="rol",
+    *         in="query",
+    *         description="Rol",
+    *         required=true,        
+    *     ),
+    *     @OA\Response(
+    *         response=201,
+    *         description="Registro de usuarios"
+    *     ),
+    *     @OA\Response(
+    *         response="default",
+    *         description="Ha ocurrido un error."
+    *     )
+    * )
+    */
+
     public function signup(Request $request)
     {
         $request->validate([
@@ -50,6 +125,39 @@ class userController extends Controller
             'message' => 'Successfully created user!'], 201);
     }
 
+
+/**
+    * @OA\POST(
+    *     path="api/login",
+    *     summary="Login usuarios",
+    *     @OA\Parameter(
+    *         name="Email",
+    *         in="query",
+    *         description="Correo electronico",
+    *         required=true,        
+    *     ),
+    *     @OA\Parameter(
+    *         name="Password",
+    *         in="query",
+    *         description="Contraseña",
+    *         required=true,        
+    *     ),
+    *     @OA\Parameter(
+    *         name="remember_me",
+    *         in="query",
+    *         description="Recordarme",
+    *     ),
+    *     @OA\Response(
+    *         response=200,
+    *         description="Ok"
+    *     ),
+    *     @OA\Response(
+    *         response="401",
+    *         description="No autorizado"
+    *     )
+    * )
+    */
+
     public function login(Request $request)
     {
         $request->validate([
@@ -81,12 +189,34 @@ class userController extends Controller
         ], 200);
     }
 
+       /**
+         * @OA\GET(
+         *     path="api/logout",
+         *     summary="Logout usuarios",
+         *     @OA\Response(
+         *         response=200,
+         *         description="Ok"
+         *     )
+         * )
+         */
+
     public function logout(Request $request)
     {
         $request->user()->token()->revoke();
         return response()->json(['message' =>
             'Successfully logged out']);
     }
+
+        /**
+    * @OA\GET(
+    *     path="api/user",
+    *     summary="User",
+    *     @OA\Response(
+    *         response=200,
+    *         description="Ok"
+    *     )
+    * )
+    */
 
     public function user(Request $request)
     {
